a6ad659ee66f04c6c43a25f92de56a9546a0f9bf
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2023-07-23 23:52:09 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -95,7 +95,7 @@ typedef struct excuteinfo
 	int status;
 } info_t;
 
-#define INPUT_INT \
+#define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
