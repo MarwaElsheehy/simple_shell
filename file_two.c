a3eea0d17d3aaa7c34a183213dccb9732ef2e340
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2023-07-23 19:55:13 +0300

    Update file_two.c

--- file_two.c
+++ file_two.c
@@ -2,43 +2,43 @@
 
 /**
  * compile - rid of vars of the parameters
- * @I: heading of struct
+ * @i: heading of struct
  * @v: the heading of zeros
  *
  * Return: if success 0
  */
-int compile(info_t *I, char **v)
+int compile(info_t *i, char **v)
 {
 	ssize_t y = 0;
 	int t = 0;
 
 	while (y != -1 && t != -2)
 	{
-		remove_data(I);
-		if (honest(I))
+		remove_data(i);
+		if (honest(i))
 			_lay("$ ");
 		_design(GUST_BULK);
-		y = take_chip(I, NULL, NULL);
+		y = take_chip(i);
 		if (y != -1)
 		{
-			suit_data(I, v);
-			t = detect_formation(I);
+			suit_data(i, v);
+			t = detect_formation(i);
 			if (t == -1)
-				detect_lead(I);
+				detect_lead(i);
 		}
-		else if (honest(I))
+		else if (honest(i))
 			_force('\n');
-		rid_data(I, 0);
+		rid_data(i, 0);
 	}
-	boost_record(I);
-	rid_data(I, 1);
-	if (!honest(I) && I->status)
-		exit(I->status);
+	boost_record(i);
+	rid_data(i, 1);
+	if (!honest(i) && i->status)
+		exit(i->status);
 	if (t == -2)
 	{
-		if (I->err_num == -1)
-			exit(I->status);
-		exit(I->err_num);
+		if (i->err_num == -1)
+			exit(i->status);
+		exit(i->err_num);
 	}
 	return (t);
 }
@@ -76,42 +76,42 @@ int detect_formation(info_t *a)
 
 /**
  * detect_lead - review if it is a deli or not
- * @Q: argument include temple used to preserve of  mission  model
+ * @j: argument include temple used to preserve of  mission  model
  *
  * Return: Always 0
  */
-void detect_lead(info_t *Q)
+void detect_lead(info_t *j)
 {
 	char *h = NULL;
 	int x, k;
 
-	Q->path = Q->argv[0];
-	if (Q->linecount_flag == 1)
+	j->path = j->argv[0];
+	if (j->linecount_flag == 1)
 	{
-		Q->line_count++;
-		Q->linecount_flag = 0;
+		j->line_count++;
+		j->linecount_flag = 0;
 	}
-	for (x = 0, k = 0; Q->arg[x]; x++)
-		if (!be_locate(Q->arg[x], " \t\n"))
+	for (x = 0, k = 0; j->arg[x]; x++)
+		if (!be_locate(j->arg[x], " \t\n"))
 			k++;
 	if (!k)
 		return;
 
-	h = detect_route(Q, _takethough(Q, "PATH="), Q->argv[0]);
+	h = detect_route(j, _takethough(j, "PATH="), j->argv[0]);
 	if (h)
 	{
-		Q->path = h;
-		spine_command(Q);
+		j->path = h;
+		spine_command(j);
 	}
 	else
 	{
-		if ((honest(Q) || _takethough(Q, "PATH=")
-			|| Q->argv[0][0] == '/') && be_lead(Q, Q->argv[0]))
-			spine_command(Q);
-		else if (*(Q->arg) != '\n')
+		if ((honest(j) || _takethough(j, "PATH=")
+			|| j->argv[0][0] == '/') && be_lead(j, j->argv[0]))
+			spine_command(j);
+		else if (*(j->arg) != '\n')
 		{
-			Q->status = 127;
-			press_false(Q, "not found\n");
+			j->status = 127;
+			press_false(j, "not found\n");
 		}
 	}
 }
@@ -129,6 +129,7 @@ void spine_command(info_t *q)
 	w = fork();
 	if (w == -1)
 	{
+		
 		perror("Error:");
 		return;
 	}
@@ -141,7 +142,7 @@ void spine_command(info_t *q)
 				exit(126);
 			exit(1);
 		}
-
+		
 	}
 	else
 	{
