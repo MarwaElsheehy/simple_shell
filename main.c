51f40e2cd427e000d506aedfddf6445f19ee5079
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2023-07-27 02:20:18 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,47 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * main - the main file
+ * @c: a char
+ * @v: a number
  *
- * Return: 0 on success, 1 on error
+ * Return: Always 0
  */
-int main(int ac, char **av)
+	unsigned int c;
+	char **v = (void*)(intptr_t)
+	info_t data[] = { INFO_INIT };
+        int fd;
+	fd = 2;
+int main()
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
 		: "=r" (fd)
 		: "r" (fd));
 
-	if (ac == 2)
+	if (c == 2)
 	{
-		fd = open(av[1], O_RDONLY);
+		fd = open(v[1], O_RDONLY);
 		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
+				_place(v[0]);
+				_place(": 0: Can't open ");
+				_place(v[1]);
+				_design('\n');
+				_design(GUST_BULK);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		info->readfd = fd;
+		data->readfd = fd;
 	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
+	reside_though_stream(data);
+	stay_record(data);
+	compile(data, v);
 	return (EXIT_SUCCESS);
 }
