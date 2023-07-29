dc6a0132600d0a78622330c1eeadaa62c500baa4
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2023-07-30 02:20:20 +0300

    Update file_eighteen.c

--- file_eighteen.c
+++ file_eighteen.c
@@ -1,90 +1,96 @@
 #include "shell.h"
 
 /**
- *  _though - starts the information data
- * @a: argument include temple used to preserve of  mission  model
- * Return: if success 0
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
-int _though(info_t *a)
+int _myenv(info_t *info)
 {
-	press_stream_chain(a->env);
+	print_list_str(info->env);
 	return (0);
 }
 
 /**
- * _takethough - transform the chain to int
- * @b: argument include temple used to preserve of  mission  model
- * @x: a character
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
  *
  * Return: the value
  */
-char *_takethough(info_t *b, const char *x)
+char *_getenv(info_t *info, const char *name)
 {
-	list_t *n = b->env;
-	char *f;
+	list_t *node = info->env;
+	char *p;
 
-	while (n)
+	while (node)
 	{
-		f = begins_with(n->c, x);
-		if (f && *f)
-			return (f);
-		n = n->close;
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
 	}
 	return (NULL);
 }
 
 /**
- * response - the heading of real pointer and zero values
- * @g: argument include temple used to preserve of  mission  model
- *  Return: if success 0
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
  */
-int response(info_t *g)
+int _mysetenv(info_t *info)
 {
-	if (g->argc != 3)
+	if (info->argc != 3)
 	{
-		_place("Incorrect number of arguements\n");
+		_eputs("Incorrect number of arguements\n");
 		return (1);
 	}
-	if (_suitthough(g, g->argv[1], g->argv[2]))
+	if (_setenv(info, info->argv[1], info->argv[2]))
 		return (0);
 	return (1);
 }
 
 /**
- *  unresponse - review if it is a deli or not
- * @m: argument include temple used to preserve of  mission  model
- *  Return: if success 0
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
  */
-int unresponse(info_t *m)
+int _myunsetenv(info_t *info)
 {
-	int j;
+	int i;
 
-	if (m->argc == 1)
+	if (info->argc == 1)
 	{
-		_place("Too few arguements.\n");
+		_eputs("Too few arguements.\n");
 		return (1);
 	}
-	for (j = 1; j <= m->argc; j++)
-		_unsuitthough(m, m->argv[j]);
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
 
 	return (0);
 }
 
 /**
- * reside_though_streamt - starts the information data
- * @p: argument include temple used to preserve of  mission  model
- * Return: if success 0
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
  */
-int reside_though_streamt(info_t *p)
+int populate_env_list(info_t *info)
 {
-	list_t *n = NULL;
-	size_t k;
+	list_t *node = NULL;
+	size_t i;
 
-	for (k = 0; environ[k]; k++)
-		put_nodule_close(&n, environ[k], 0);
-	p->env = n;
+	for (i = 0; environ[i]; i++)
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
 	return (0);
 }
+
 
 
 
