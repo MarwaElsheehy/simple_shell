a0ddb8f9f7c8a21487ebe641d7e564df2ad704e3
Marwa <118805027+MarwaElsheehy@users.noreply.github.com>
2023-08-09 10:03:09 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,16 @@
+#include "shell.h"
+
 /**
- * main - the main file
- * @ac: a char
- * @av: a number
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: Always 0
+ * Return: 0 on success, 1 on error
  */
 int main(int ac, char **av)
 {
-	// unsigned int ac;
-	// char **av = (void*)(intptr_t)
 	info_t info[] = { INFO_INIT };
-        int fd =2;
+	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
@@ -19,20 +19,26 @@ int main(int ac, char **av)
 
 	if (ac == 2)
 	{
-		fd = open(v[1], O_RDONLY);
+		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
-			exit(126);
+		{
+			if (errno == EACCES)
+				exit(126);
 			if (errno == ENOENT)
 			{
-				_place(av[0]);
-				_place(": 0: Can't open ");
-				_place(av[1]);
-				_design('\n');
-				_design(GUST_BULK);
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
 				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+		info->readfd = fd;
 	}
-	reside_though_stream(info);
-	stay_record(info);
-	compile(info, av);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
